2c7a809f7963b0b5c714732383b75e2596b7a96f
vet3whale <githubvetri@gmail.com>
2026-04-08 12:23:33 +0800

    Revert "Rewrite telegram_tx to follow library convention and fix tone setting"
    This reverts commit 630e826fff797b5e60a1ab1587d8fbb3febb800d.

--- FinalProjectESP32/main/telegram_tx.h
+++ FinalProjectESP32/main/telegram_tx.h
@@ -2,6 +2,5 @@
 void Telegram_Init();
 void vTelegramTask(void *pvParameters);
 String Telegram_GetPersonality();
-bool Telegram_IsPersonalitySet();
 #define TELEGRAM_TASK_STACK_SIZE 8192
 #define TELEGRAM_TASK_PRIORITY   2
